96e89fdf4d344ca97e701d6b3648978b8a1a0d56
danio86 <d.harder@gmx.at>
2022-09-30 20:11:00 +0000

    changed game idea

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-console.log('yes!')
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d')
+
+draw();
+
+function draw() {
+    ctx.fillstyle = 'black';
+    ctx.fillRect(10, 20, 20, 20);
+};
+
+
+function gameloop() {
+
+};
+
