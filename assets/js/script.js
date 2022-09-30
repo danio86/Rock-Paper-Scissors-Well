952aff67c38d8a03b86a8f8538d5673fa205ff74
danio86 <d.harder@gmx.at>
2022-09-30 20:40:14 +0000

    fixed erros

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,11 @@
-let canvas = document.getElementById('canvas');
-let ctx = canvas.getContext('2d')
 
+let canvas = document.getElementById('game-area');
+let ctx = canvas.getContext('2d');
+       
 draw();
 
 function draw() {
-    ctx.fillstyle = 'black';
+    ctx.fillStyle = 'yellow';
     ctx.fillRect(10, 20, 20, 20);
-};
-
-
-function gameloop() {
-
-};
-
+}
+            
