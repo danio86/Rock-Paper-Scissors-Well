ae5897b7436813f30965e9f2826cad600f115406
danio86 <d.harder@gmx.at>
2022-10-04 08:49:35 +0000

    add all variables to js, and addCube function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,34 @@
-
 let canvas = document.getElementById('game-area');
 let ctx = canvas.getContext('2d');
-       
-draw();
+//ctx is the snake. getContext(2d) allows us to drwaw in 2d in the cavas(game-area)
+let snake = [
+    {x: 2, y:2},
+    {x: 3, y:2}, 
+    {x: 4, y:2}
+];
+//an array of jsons is 1 element/value
+let food = {x: 60, y:60}; 
+//{x:..,y:..}this is 1 value called json! withs this a var can have "2" values!
+let direction = 'LEFT';
+let row = 10;
+let col = 10;
+let cellWidth = canvas.width/col;
+let cellHight = canvas.height/row;
+
+
+function addCube(x ,y) {
+    ctx.fillRect(x, y, 9, 9);
+};
 
 function draw() {
     ctx.fillStyle = 'yellow';
-    ctx.fillRect(10, 20, 20, 20);
+    addCube(10, 20);
+    addCube(20, 20);
+    addCube(30, 20);
+    addCube(40, 20);
+
+    ctx.fillStyle = 'red';
+    addCube(50, 50);
 }
-            
+
+draw();          
