203488ad22f154e3d8ea008928c49c0c5e9632ac
danio86 <d.harder@gmx.at>
2022-10-04 09:41:57 +0000

    add raster to canvas and adapded objects to it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,38 @@
+//Game Area:
 let canvas = document.getElementById('game-area');
 let ctx = canvas.getContext('2d');
 //ctx is the snake. getContext(2d) allows us to drwaw in 2d in the cavas(game-area)
+
 let snake = [
     {x: 2, y:2},
     {x: 3, y:2}, 
     {x: 4, y:2}
 ];
 //an array of jsons is 1 element/value
-let food = {x: 60, y:60}; 
+let food = {x: 5, y: 5}; 
 //{x:..,y:..}this is 1 value called json! withs this a var can have "2" values!
 let direction = 'LEFT';
-let row = 10;
-let col = 10;
+
+let row = 30;
+let col = 30;
 let cellWidth = canvas.width/col;
 let cellHight = canvas.height/row;
+//this creates a raster with cells
 
 
 function addCube(x ,y) {
-    ctx.fillRect(x, y, 9, 9);
+    ctx.fillRect(x*cellWidth, y*cellHight, cellWidth-1, cellHight-1);
+    //...(x-pos, y-pos, width, height) of painted object
 };
 
 function draw() {
     ctx.fillStyle = 'yellow';
-    addCube(10, 20);
-    addCube(20, 20);
-    addCube(30, 20);
-    addCube(40, 20);
+    addCube(snake.x, snake.y)
+    
 
+    //food!
     ctx.fillStyle = 'red';
-    addCube(50, 50);
+    addCube(food.x, food.y);
 }
 
 draw();          
